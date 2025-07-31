ad03631a14605151f69f0a0cbdb9199c895cd51d
MRITYUNJAY <kumarmritunjay830@gmail.com>
2025-07-31 17:05:38 +0530

    RG+PROVIDER

---
+++ Rgcode.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "mrityunjayrg" {
+  name     = "MkRg"
+  location = "West Europe"
+}
---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "cfb96b56-ec85-4b4e-af54-57486d3edffc"
+}
